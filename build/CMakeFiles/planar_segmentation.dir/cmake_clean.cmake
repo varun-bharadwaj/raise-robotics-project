a4b0f77a56661e57fed2b50928f4af2f3518ac5e
Varun Bharadwaj <varun.bharadwaj11@gmail.com>
2021-12-28 23:52:32 -0800

    Uploading Files to GitHub

---
+++ build/CMakeFiles/planar_segmentation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/planar_segmentation.dir/planar_segmentation.cpp.o"
+  "planar_segmentation"
+  "planar_segmentation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/planar_segmentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
